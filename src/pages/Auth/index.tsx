cf41149772c1c330d66d1b0c98cf95ea9d8b1f03
Rodion <www.rodionkochetkow@gmail.com>
2021-06-16 22:35:31 +0300

    feat: add favicon

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -2,7 +2,6 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 // import ChangeLang from 'components/ChangeLang';
 import Logo from 'components/Logo';
 import Login from './Login';
-import ForgotPassword from './ForgotPassword';
 import useStyles from './style';
 
 const Auth = () => {
@@ -12,9 +11,8 @@ const Auth = () => {
       <Logo />
       {/* <ChangeLang /> */}
       <Switch>
-        <Route exact path="/auth/login" component={Login} />
-        <Route exact path="/auth/forgot" render={() => <ForgotPassword />} />
-        <Redirect to="/auth/login" />
+        <Route exact path="/auth" component={Login} />
+        <Redirect to="/auth" />
       </Switch>
     </div>
   );
--- src/pages/Auth/style.ts
+++ src/pages/Auth/style.ts
@@ -10,29 +10,6 @@ const useStyles = makeStyles((theme: typeof MuiTheme) =>
       justifyContent: 'center',
       right: 0,
       top: 0,
-      '&::before': {
-        content: '""',
-        display: 'block',
-        width: '100vw',
-        height: '100vh',
-        position: 'absolute',
-        backgroundColor: '#2e3192',
-        backgroundBlendMode: 'lighten',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: ' contain',
-        backgroundPosition: 'right 0',
-        clipPath: 'polygon(20% 0, 100% 0, 100% 73%)',
-        [theme.breakpoints.down('sm')]: {
-          clipPath: 'polygon(0 0, 100% 0, 100% 12%, 0 26%)',
-          transform: 'scaleX(-1)',
-        },
-        [theme.breakpoints.up('md')]: {
-          clipPath: 'polygon(37% 0, 100% 0, 100% 65%)',
-        },
-        [theme.breakpoints.up('lg')]: {
-          clipPath: 'polygon(35% 0, 100% 0, 100% 65%)',
-        },
-      },
     },
   }),
 );
